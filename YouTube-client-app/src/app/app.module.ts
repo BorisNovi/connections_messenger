00d8647a71f57b43619b412b5d28dd580a838578
Boris Novi <borisnovikov4815@gmail.com>
2023-10-24 13:18:17 +0400

    feat: add angular material, create search header

--- YouTube-client-app/src/app/app.module.ts
+++ YouTube-client-app/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './youtube/components/header/header.component';
 import { MainComponent } from './youtube/pages/main/main.component';
 import { CardComponent } from './youtube/components/card/card.component';
+import { FilteringCriteriaBlockComponent } from './youtube/components/filtering-criteria-block/filtering-criteria-block.component';
+import { SearchResultsBlockComponent } from './youtube/components/search-results-block/search-results-block.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +20,18 @@ import { CardComponent } from './youtube/components/card/card.component';
     HeaderComponent,
     MainComponent,
     CardComponent,
+    FilteringCriteriaBlockComponent,
+    SearchResultsBlockComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+
+    // Material
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
