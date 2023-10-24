9324e34934fd267ac0d446415500af4d077de0a3
Boris Novi <borisnovikov4815@gmail.com>
2023-10-24 16:11:56 +0400

    feat: layout card component

--- YouTube-client-app/src/app/youtube/components/search-results-block/search-results-block.component.ts
+++ YouTube-client-app/src/app/youtube/components/search-results-block/search-results-block.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-results-block.component.scss']
 })
 export class SearchResultsBlockComponent {
-
+  cards = [{ name: 'a' }, { name: 'b' }, { name: 'c' }];
 }
