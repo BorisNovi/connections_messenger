5f12a586ea5625bb487868107b32bef37e665e47
Boris Novi <borisnovikov4815@gmail.com>
2023-11-07 22:02:48 +0400

    feat: add lasy load for youtube and auth module

--- YouTube-client-app/src/app/app-routing.module.ts
+++ YouTube-client-app/src/app/app-routing.module.ts
@@ -1,26 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainComponent } from './youtube/pages/main/main.component';
 import { NotFoundComponent } from './core/components/not-found/not-found.component';
 import { ShellComponent } from './core/shell/shell.component';
-import { DetailComponent } from './youtube/components/detail/detail.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ShellComponent,
     children: [
-      { path: '', component: MainComponent },
-      { path: 'detail/:id', component: DetailComponent },
+      { path: '', loadChildren: () => import('./youtube/youtube.module').then((m) => m.YoutubeModule) },
+      { path: 'login', loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule) },
       { path: '404', component: NotFoundComponent },
       { path: '**', redirectTo: '404' }
     ]
-  },
-  { path: '**', redirectTo: '404' },
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- YouTube-client-app/src/app/youtube/youtube.module.ts
+++ YouTube-client-app/src/app/youtube/youtube.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { RouterModule, Routes, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { CardComponent } from './components/card/card.component';
 import { MainComponent } from './pages/main/main.component';
@@ -11,7 +11,12 @@ import { SearchPipe } from './pipes/search.pipe';
 import { SortPipe } from './pipes/sort.pipe';
 import { FilterPipe } from './pipes/filter.pipe';
 import { CustomButtonComponent } from '../shared/components/custom-button/custom-button.component';
-import { DetailComponent } from './components/detail/detail.component';
+import { DetailComponent } from './pages/detail/detail.component';
+
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'detail/:id', component: DetailComponent },
+];
 
 @NgModule({
   declarations: [
@@ -30,6 +35,7 @@ import { DetailComponent } from './components/detail/detail.component';
     RouterLink,
     MatIconModule,
     MatButtonModule,
+    RouterModule.forChild(routes),
   ],
   exports: [
   ]
