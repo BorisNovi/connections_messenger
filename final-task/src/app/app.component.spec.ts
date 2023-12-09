4cee1e3697ce9b93ee2e4494f8f328f785afe678
Boris Novi <borisnovikov4815@gmail.com>
2023-12-09 13:50:57 +0400

    feat: add eslint-airbnb and angular material

--- final-task/src/app/app.component.spec.ts
+++ final-task/src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'final-task'`, () => {
+  it('should have as title \'final-task\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('final-task');
