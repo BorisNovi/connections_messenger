d6057b6b6556ef04ea03d627792e8a8aac016e6b
Boris Novi <borisnovikov4815@gmail.com>
2023-12-09 18:22:31 +0400

    feat: add form with validators to register page

--- final-task/src/app/registration/components/registration-form/registration-form.component.ts
+++ final-task/src/app/registration/components/registration-form/registration-form.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiRegistrationService } from '../../services/api-registration.service';
 
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
   styleUrls: ['./registration-form.component.scss']
 })
-export class RegistrationFormComponent {
+export class RegistrationFormComponent implements OnInit {
+  public registerForm!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private registrationService: ApiRegistrationService,
+  ) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      name: ['', [Validators.required, Validators.maxLength(40), Validators.pattern('^[a-zA-Z\\s]+$')]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$')]]
+    });
+  }
+
+  isHidden = true;
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      this.registerForm.reset();
+      // register
+      // this.loginService.updLoginCredentials({
+      //   login: this.credentials.value.login || null,
+      //   password: this.credentials.value.password || null
+      // });
+    }
+  }
 }
--- final-task/src/app/registration/registration.module.ts
+++ final-task/src/app/registration/registration.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
 
 const routes: Routes = [
   { path: '', component: RegistrationFormComponent },
+];
+
+const MATERIAL_IMPORTS = [
+  MatIconModule,
+  MatButtonToggleModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
 ];
 
 @NgModule({
@@ -13,7 +27,10 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes),
+    MATERIAL_IMPORTS
   ]
 })
 export class RegistrationModule { }
