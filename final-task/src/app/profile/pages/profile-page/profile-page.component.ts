6d9445396124388122c5af30d1c6a0534a66d755
Boris Novi <borisnovikov4815@gmail.com>
2023-12-11 20:05:14 +0400

    add profile layout

--- final-task/src/app/profile/pages/profile-page/profile-page.component.ts
+++ final-task/src/app/profile/pages/profile-page/profile-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, DestroyRef, OnInit } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ApiProfileService } from '../../services/api-profile.service';
+import { IProfileResponse } from '../../models/prifile-response.model';
 
 @Component({
   selector: 'app-profile-page',
@@ -8,12 +10,37 @@ import { ApiProfileService } from '../../services/api-profile.service';
   styleUrls: ['./profile-page.component.scss']
 })
 export class ProfilePageComponent implements OnInit {
-  constructor(private apiProfileService: ApiProfileService, private destroyRef: DestroyRef,) {}
+  public nameForm!: FormGroup;
+  userData!: IProfileResponse;
+  isUpdatable = false;
+
+  constructor(
+    private apiProfileService: ApiProfileService,
+    private destroyRef: DestroyRef,
+    private formBuilder: FormBuilder,
+  ) {}
+
   ngOnInit(): void {
+    this.nameForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+    });
+
     this.apiProfileService.getProfileData()
       .pipe(
         takeUntilDestroyed(this.destroyRef)
       )
-      .subscribe((data) => console.log(data));
+      .subscribe((data) => {
+        this.userData = data;
+      });
+  }
+
+  updateName(): void {
+    this.isUpdatable = !this.isUpdatable;
+  }
+
+  onSubmit(): void {
+    if (this.nameForm.valid) {
+      console.log(this.nameForm.value);
+    }
   }
 }
--- final-task/src/app/profile/profile.module.ts
+++ final-task/src/app/profile/profile.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
 const routes: Routes = [
   { path: '', component: ProfilePageComponent },
+];
+
+const MATERIAL_IMPORTS = [
+  MatIconModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatListModule,
+  MatDividerModule
 ];
 
 @NgModule({
@@ -13,7 +29,10 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes),
+    MATERIAL_IMPORTS
   ]
 })
 export class ProfileModule { }
