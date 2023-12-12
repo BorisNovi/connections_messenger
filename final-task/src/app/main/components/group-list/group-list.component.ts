553fcd6e931897aaac53a09223d4163b450460ae
Boris Novi <borisnovikov4815@gmail.com>
2023-12-12 19:57:55 +0400

    feat: add store to get group list, change layout, add functional remove

--- final-task/src/app/main/components/group-list/group-list.component.ts
+++ final-task/src/app/main/components/group-list/group-list.component.ts
@@ -2,9 +2,12 @@ import { Component, DestroyRef, OnInit } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Store } from '@ngrx/store';
 import { setGroupListItems } from 'src/app/NgRx/actions/group-list.action';
+import { selectGroupListItems } from 'src/app/NgRx/selectors/group-list.selector';
+import { catchError, of, switchMap } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LocalService } from 'src/app/core/services/local.service';
 import { ApiGroupListService } from '../../services/api-group-list.service';
 import { IGroupItem, IGroupListResponse } from '../../models/group-list-response.model';
-import { selectGroupListItems } from 'src/app/NgRx/selectors/group-list.selector';
 
 @Component({
   selector: 'app-group-list',
@@ -12,12 +15,17 @@ import { selectGroupListItems } from 'src/app/NgRx/selectors/group-list.selector
   styleUrls: ['./group-list.component.scss']
 })
 export class GroupListComponent implements OnInit {
+  delay = 2000;
+  isDeleteDisabled = false;
   groupList!: IGroupItem[];
+  myUid = this.localService.getData('uid');
 
   constructor(
     private apiGroupListService: ApiGroupListService,
     private destroyRef: DestroyRef,
     private store: Store,
+    private snackBar: MatSnackBar,
+    private localService: LocalService
   ) {}
 
   ngOnInit(): void {
@@ -25,14 +33,20 @@ export class GroupListComponent implements OnInit {
   }
 
   getGroupList(): void {
-    this.store.select(selectGroupListItems).subscribe((data) => console.log(data));
-    this.apiGroupListService.getGroupList()
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe(((data) => {
-        console.log('Groups list', data);
+    this.store.select(selectGroupListItems)
+      .pipe(
+        switchMap((groupListItems) => (groupListItems[0]
+          ? of({ Items: groupListItems })
+          : this.apiGroupListService.getGroupList())),
+        catchError((err) => {
+          this.openSnackBar(err.error.message || 'No Internet connection!');
+          return of();
+        }),
+        takeUntilDestroyed(this.destroyRef)
+      ).subscribe((data) => {
         this.groupList = data.Items;
         this.store.dispatch(setGroupListItems({ groupItems: data.Items }));
-      }));
+      });
   }
 
   testCreate() {
@@ -41,9 +55,27 @@ export class GroupListComponent implements OnInit {
       .subscribe(((data) => console.log(data)));
   }
 
-  testDelete() {
-    this.apiGroupListService.deleteGroup('dvardjpps4c')
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe(((data) => console.log(data)));
+  deleteGroup(groupID: string): void {
+    this.isDeleteDisabled = true;
+    this.openDeleteSnackBar(); // Добавть экшн снекбара
+    this.apiGroupListService.deleteGroup(groupID)
+      .pipe(catchError((err) => {
+        this.isDeleteDisabled = false;
+        this.openSnackBar(err.error.message || 'No Internet connection!');
+        return of();
+      }), takeUntilDestroyed(this.destroyRef))
+      .subscribe((() => {
+        this.isDeleteDisabled = false;
+        this.openSnackBar('Group deleted successfuly!');
+        // this.store.dispatch(deleteGroupListItem({ groupItem: groupID })); Написать
+      }));
+  }
+
+  openDeleteSnackBar(): void {
+    this.snackBar.open('Delete group?', 'Yes', { verticalPosition: 'top', horizontalPosition: 'left' });
+  }
+
+  openSnackBar(message: string): void {
+    this.snackBar.open(message, 'Ok', { duration: this.delay });
   }
 }
